03677c2b2938da4defc194b7a8dad9c25c037f69
merziy <alexzandermathis@gmail.com>
2022-01-11 17:53:20 -0500

    Started building new portfolio on React. Currently working on CSS Grid and Responsive Web Design issues

---
+++ portfolio/src/pages/blog.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="blog">Blog</div>;
+};
+
+export default Home;
---
+++ portfolio/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Home;
