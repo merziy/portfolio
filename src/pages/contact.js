56fe766a7bae9bce5230b68cc800411fca4da462
merziy <alexzandermathis@gmail.com>
2022-01-27 13:37:43 -0500

    Created links on Contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,9 +4,32 @@ const Home = () => {
   return (
     <div className="contact">
       <section id="contact-info">
-        <p>email: alexzandermathis@gmail.com</p>
-        <p>phone: 336-448-9778</p>
-        <p>linkedin: linkedin.com/i/azmathis</p>
+        <p>
+          email:{" "}
+          <a
+            href="mailto: alexzandermathis@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            alexzandermathis@gmail.com
+          </a>
+        </p>
+        <p>
+          Phone:{" "}
+          <a href="tel: 336-448-9778" target="_blank" rel="noreferrer">
+            336-448-9778
+          </a>
+        </p>
+        <p>
+          linkedin:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/azmathis/"
+          >
+            azmathis
+          </a>
+        </p>
       </section>
     </div>
   );
