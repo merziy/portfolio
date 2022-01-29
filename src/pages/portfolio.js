9cf45a6f3d3d0f1d1e9d2a76bec25a7309a297bd
merziy <alexzandermathis@gmail.com>
2022-01-29 11:36:39 -0500

    Added new React/Go project.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,30 +7,6 @@ const Home = () => {
         <h1>Portfolio</h1>
       </section>
       <section id="links">
-        {/* <figure>
-          <img src="https://via.placeholder.com/250" alt="design" />
-          <figcaption>Image</figcaption>
-        </figure>
-        <figure>
-          <img src="https://via.placeholder.com/250" alt="design" />
-          <figcaption>Image</figcaption>
-        </figure>
-        <figure>
-          <img src="https://via.placeholder.com/250" alt="design" />
-          <figcaption>Image</figcaption>
-        </figure>
-        <figure>
-          <img src="https://via.placeholder.com/250" alt="design" />
-          <figcaption>Image</figcaption>
-        </figure>
-        <figure>
-          <img src="https://via.placeholder.com/250" alt="design" />
-          <figcaption>Image</figcaption>
-        </figure>
-        <figure>
-          <img src="https://via.placeholder.com/250" alt="design" />
-          <figcaption>Image</figcaption>
-        </figure> */}
         <figure>
           <a
             href="https://codepen.io/merziy/pen/YzEKgmy"
@@ -55,6 +31,21 @@ const Home = () => {
           </a>
           <figcaption>
             Midnight Confessions site (PHP, Docker, jQuery)
+          </figcaption>
+        </figure>
+        <figure>
+          <a
+            href="https://github.com/merziy/go-react-chat-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              src={require("../assets/go-react-chat-app.webp")}
+              alt="Chat app built with React and Go"
+            ></img>
+          </a>
+          <figcaption>
+            Realtime Chat Application (Go, React, Docker, Sass)
           </figcaption>
         </figure>
       </section>
