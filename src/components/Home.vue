68a00c0e6c4c75bee7a370e3ada21487740f1c04
Alexander Mathis <=>
2023-06-09 21:25:56 -0400

    Started figuring this Vue and Tailwind thing out...

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="bg-gray-100 min-h-screen flex items-center justify-center">
-        <section class="hero">
-            <h1>Home</h1>
+    <div class="bg-gray-100 min-h-screen flex flex-col">
+        <Navigation />
+        <section class="hero mx-auto">
+            <h1 class="md:mx-5 ">Alexander Mathis</h1>
+            <p>Alexander Mathis is a front end engineer based in New York City. He is currently working with Juicyorange and helping clients create e-commerce solutions to suit their clients needs.</p>
+            <p>Ready to talk? Select the phone or email icons below and we can work together.</p>
         </section>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navigation from "./Nav.vue";
+components: {
+    Navigation
+};
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="">
+        <ul class="flex border-b">
+            <li class="-mb-px mr-1">
+                <a class="inline-block py-2 px-4 text-blue-700 font-semibold" href="#">Portfolio</a>
+            </li>
+            <li class="-mb-px mr-1">
+                <a class="inline-block py-2 px-4 text-blue-700 font-semibold" href="#">Contact</a>
+            </li>
+            <li class="-mb-px mr-1">
+                <a class="inline-block py-2 px-4 text-blue-700 font-semibold" href="#">Bio</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navigation', 
+}
+</script>
