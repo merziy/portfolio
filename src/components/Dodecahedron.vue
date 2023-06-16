adafaa4bac3137bbc64ab11f9fb4e09522b8f372
Alexander <alexzandermathis@gmail.com>
2023-06-15 20:46:37 -0400

    Enabled PBR

--- src/components/Dodecahedron.vue
+++ src/components/Dodecahedron.vue
@@ -1,3 +1,8 @@
+<!--
+  A great help in writing this component, besides the offical Three docs,
+  was the book at https://discoverthreejs.com/. Thanks for explaining 
+  PBR (Physically Based Rendering [and Lighting]) :) 
+-->
 <template>
     <canvas id="canvas" ref="canvas"></canvas>
 </template>
@@ -22,16 +27,24 @@ export default {
         1000
       );
       const renderer = new THREE.WebGLRenderer({ canvas: this.$refs.canvas as HTMLCanvasElement, alpha: true });
+      // renderer.useLegacyLights = true; // Maybe I dont need this?
       renderer.setSize(window.innerWidth, window.innerHeight);
       renderer.setPixelRatio(window.devicePixelRatio); // for HiDPI displays
+      
+      function createLights() {
+        const light = new THREE.DirectionalLight('white', 8);
+        light.position.set( 10,10,10 );
+        return light;
+      }
 
       const geometry = new THREE.DodecahedronGeometry(1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const material = new THREE.MeshStandardMaterial({ color: 'purple' });
       const dodecahedron = new THREE.Mesh(geometry, material);
-      scene.add(dodecahedron);
+      const light = createLights();
+      scene.add(dodecahedron, light);
 
       camera.position.z = 5;
-
+      
       const animate = () => {
         requestAnimationFrame(animate);
         dodecahedron.rotation.x += 0.01;
